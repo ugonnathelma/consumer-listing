4e0a32aa7e403a64a65089442d8b2a90399d4c3d
Ugonna Ofoegbu <ugonnaofoegbu@MacBook-Pro.fritz.box>
2019-12-31 21:03:59 +0100

    Updates

---
+++ precache-manifest.bc5ece5831654f82ac7975f04dcc2eea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b2686d18679f031a86f16f90ffdd85",
+    "url": "/consumer-listing/index.html"
+  },
+  {
+    "revision": "4c4f38fd51a80316854a",
+    "url": "/consumer-listing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "85c361f610d204aba566",
+    "url": "/consumer-listing/static/js/2.144348c4.chunk.js"
+  },
+  {
+    "revision": "0c559e1ae0a9426ead1485b442d61f0b",
+    "url": "/consumer-listing/static/js/2.144348c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c4f38fd51a80316854a",
+    "url": "/consumer-listing/static/js/main.05a48229.chunk.js"
+  },
+  {
+    "revision": "a31641d1ba3e5d9d59db",
+    "url": "/consumer-listing/static/js/runtime-main.c169d3e6.js"
+  },
+  {
+    "revision": "94e2e8a0a24aac8a40c294bb93491b75",
+    "url": "/consumer-listing/static/media/close.94e2e8a0.svg"
+  },
+  {
+    "revision": "96386f9a973191f0bae5800218e05210",
+    "url": "/consumer-listing/static/media/loading-gif.96386f9a.gif"
+  },
+  {
+    "revision": "2188b2694026f40f7b2841eec1fd21c7",
+    "url": "/consumer-listing/static/media/logo.2188b269.svg"
+  }
+]);
